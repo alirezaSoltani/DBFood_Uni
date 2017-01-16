e24d4dc15dfd4e19aa2526238dc88a3a1705e0e0
alirezaSoltani <alireza.soltani1373@gmail.com>
2017-01-16 10:13:37 -0800

    init_Alireza

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "sql6.freemysqlhosting.net";
+$username = "sql6153901";
+$password = "R4kN1S4jJH";
+$dbname = "sql6153901";
+$conn = new mysqli($servername , $username , $password , $dbname);
+//show eror
+if($conn->connect_error){
+    die("Connection failed : ".$conn->connect_error);
+}
+?>
